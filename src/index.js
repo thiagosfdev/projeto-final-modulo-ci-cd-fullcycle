313ebefe1fa95eeef2193243a190e7637eff14d6
Thiago Soares Figueiredo <dev01.thiagosf@gmail.com>
2021-10-13 00:04:27 -0300

    fix: ⚡ fix bug
    config file properties

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const operations = require('./operations')
+
 const express = require('express')
 
 const api = express()
+
 const hostname = 'localhost'
+
 const port = 3000
 
 api.get('/', (_req, res) => res.json({ api: 'OK' }))
 
-const soma = operations.sum(10, 20)
+operations.div(10, 20)
+
+operations.mod(10, 20)
+
+operations.mult(10, 20)
+
+operations.sub(10, 20)
+
+operations.sum(10, 20)
 
 api.listen(port, () => {
+
   console.log(`Server running at http://${hostname}:${port}`)
-  console.log('soma :>> ', soma);
+  
 })
