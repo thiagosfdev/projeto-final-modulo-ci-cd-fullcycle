e7100b4257a32ad8edfd4c410580120cc366734b
Thiago Soares Figueiredo <dev01.thiagosf@gmail.com>
2021-10-13 00:32:36 -0300

    test: 👁️‍🗨️tests
    unit tests

--- src/operations.test.js
+++ src/operations.test.js
@@ -1,5 +1,25 @@
 const operations = require('./operations')
 
-test('add 1 + 2 to be equal 3', () => {
-  expect(operations.sum(1, 2)).toBe(3)
+describe('Test operations: ', () => {
+
+  test('add 1 + 2 to be equal 3', () => {
+    expect(operations.sum(1, 2)).toBe(3)
+  })
+
+  test('sub 2 - 1 to be equal 1', () => {
+    expect(operations.sub(2, 1)).toBe(1)
+  })
+
+  test('mult 1 x 2 to be equal 2', () => {
+    expect(operations.mult(1, 2)).toBe(2)
+  })
+
+  test('div 10 / 5 to be equal 2', () => {
+    expect(operations.div(10, 5)).toBe(2)
+  })
+
+  test('mod 10 % 5 to be equal 0', () => {
+    expect(operations.mod(10, 5)).toBe(0)
+  })
+
 })
